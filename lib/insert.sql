e58b27b5dae5c70925623141df211f9b72ae147c
aggreyrc <aggreyronoh@gmail.com>
2024-09-09 23:37:18 +0300

    bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age,sex,color,temperament,alive) VALUES ('Mr. Chocolate', 10, 'Black', 'M','Low', 1);
+INSERT INTO bears (name, age,sex,color,temperament,alive) VALUES ('Rowdy', 12, 'Black', 'F','Low', 1);
+INSERT INTO bears (name, age,sex,color,temperament,alive) VALUES ('Tabitha', 9, 'Black', 'M','Low', 2);
+INSERT INTO bears (name, age,sex,color,temperament,alive) VALUES ('Sergeant Brown', 7, 'Black', 'M','Low', 1);
+INSERT INTO bears (name, age,sex,color,temperament,alive) VALUES ('Melissa', 17, 'Black', 'F','Low', 1);
+INSERT INTO bears (name, age,sex,color,temperament,alive) VALUES ('Grinch', 4, 'Black', 'M','Low', 1);
+INSERT INTO bears (name, age,sex,color,temperament,alive) VALUES ('Wendy', 20, 'Black', 'F','Low', 2);
+INSERT INTO bears (name, age,sex,color,temperament,alive) VALUES (NULL, 2, 'Black', 'M','Low', 1);
